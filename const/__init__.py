50c53975c8e5cc52c285d2bbf353132f5c15aee4
Korenevskiy Denis <denkoren@yandex-team.ru>
2021-07-13 08:45:29 +0300

    Fill PDF form with values making it printable

---
+++ const/__init__.py
@@ -0,0 +1,8 @@
+KEY_ANNOTATIONS = '/Annots'
+KEY_SUBTYPE = '/Subtype'
+
+ANNOT_NAME = '/T'
+ANNOT_VALUE = '/V'
+ANNOT_RECT = '/Rect'
+
+SUBTYPE_WIDGET = '/Widget'
---
+++ fill-form.py
@@ -0,0 +1,59 @@
+from typing import Dict, Optional
+
+import sys
+from datetime import date
+import json
+
+import pdfrw
+
+import const
+
+
+def read_values(values_file_path: str) -> Dict:
+    with open(values_file_path) as f:
+        field_values: Dict = json.load(f)
+
+    return field_values
+
+
+def fill_pdf(input_pdf_path, output_pdf_path, field_values: Optional[Dict] = None):
+    if field_values is None:
+        field_values = {}
+
+    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    for page in template_pdf.pages:
+        annotations = page[const.KEY_ANNOTATIONS]
+        for annotation in annotations:
+            if annotation[const.KEY_SUBTYPE] == const.SUBTYPE_WIDGET:
+
+                annotation_name = annotation.get(const.ANNOT_NAME)
+                if annotation_name is None:
+                    continue
+
+                annotation_name = annotation_name.decode()  # restore original annotation name to make comparison work
+                value = field_values.get(annotation_name)
+
+                if value is None:
+                    continue
+
+                if type(value) == bool:
+                    if value:
+                        annotation.update(pdfrw.PdfDict(
+                            AS=pdfrw.PdfName('Yes')))
+                else:
+                    annotation.update(
+                        pdfrw.PdfDict(V=str(value))
+                    )
+                    annotation.update(pdfrw.PdfDict(AP=''))
+
+    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))  # NEW
+    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+
+
+field_values_file = sys.argv[1]
+pdf_template = sys.argv[2]
+pdf_output = sys.argv[3]
+
+
+data_dict = read_values(field_values_file)
+fill_pdf(pdf_template, pdf_output, data_dict)
