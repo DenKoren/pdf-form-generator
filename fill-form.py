722802f36ec8b54f4d41c63db2fccb66886ca7f2
Korenevskiy Denis <denkoren@yandex-team.ru>
2021-07-13 20:00:21 +0300

    Support printing of filled form to stdout

--- fill-form.py
+++ fill-form.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Union, BinaryIO, AnyStr
 
 import sys
 import yaml
@@ -10,18 +10,21 @@ import pdfrw
 import const
 
 
-def read_values(values_file_path: str) -> Dict:
-    with open(values_file_path) as f:
-        data: Dict = yaml.safe_load(f)
+def read_values(values: Union[BinaryIO, AnyStr]) -> Dict:
+    if type(values) is str or type(values) is bytes:
+        with open(values) as f:
+            data: Dict = yaml.safe_load(f)
+    else:
+        data: Dict = yaml.safe_load(values)
 
     return data.get('field_values', {})
 
 
-def fill_pdf(input_pdf_path, output_pdf_path, field_values: Optional[Dict] = None):
+def fill_pdf(input_pdf, field_values: Optional[Dict] = None, output_pdf: Union[BinaryIO, AnyStr] = sys.stdout):
     if field_values is None:
         field_values = {}
 
-    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    template_pdf = pdfrw.PdfReader(input_pdf)
     for page in template_pdf.pages:
         annotations = page[const.KEY_ANNOTATIONS]
         for annotation in annotations:
@@ -48,13 +51,23 @@ def fill_pdf(input_pdf_path, output_pdf_path, field_values: Optional[Dict] = Non
                     annotation.update(pdfrw.PdfDict(AP=''))
 
     template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))  # NEW
-    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+    pdfrw.PdfWriter().write(output_pdf, template_pdf)
 
 
-field_values_file = sys.argv[1]
-pdf_template = sys.argv[2]
-pdf_output = sys.argv[3]
+pdf_form = sys.argv[1]
+form_values = sys.stdin
+if len(sys.argv) >= 3:
+    form_values = sys.argv[2]
+    if form_values == '-':
+        form_values = sys.stdin
 
+pdf_output = sys.stdout.buffer
+if len(sys.argv) >= 4:
+    pdf_output = sys.argv[3]
+    if pdf_output == '-':
+        pdf_output = sys.stdout.buffer
 
-data_dict = read_values(field_values_file)
-fill_pdf(pdf_template, pdf_output, data_dict)
+data_dict = read_values(form_values)
+fill_pdf(input_pdf=pdf_form,
+         field_values=data_dict,
+         output_pdf=pdf_output)
